b410c2b8857206aa3dea5a2e354b179d5a5484ba
Adarsh Dubey <84132532+inclinedadarsh@users.noreply.github.com>
2024-03-19 03:24:36 +0530

    feat: customize theme
    - customize button
    - customize footer
    - customize header
    - customize colors in tailwind.config

--- packages/blog-starter-kit/themes/enterprise/components/publication-logo.tsx
+++ packages/blog-starter-kit/themes/enterprise/components/publication-logo.tsx
@@ -4,42 +4,41 @@ import { useAppContext } from './contexts/appContext';
 import { PublicationFragment } from '../generated/graphql';
 
 const getPublicationLogo = (publication: PublicationFragment, isSidebar?: boolean) => {
-	if (isSidebar) {
-		return publication.preferences.logo; // Always display light mode logo in sidebar
-	}
-	return publication.preferences.darkMode?.logo || publication.preferences.logo;
+  if (isSidebar) {
+    return publication.preferences.logo; // Always display light mode logo in sidebar
+  }
+  console.log(publication.preferences.darkMode)
+  return publication.preferences.darkMode?.logo || publication.preferences.logo;
 }
 
 export const PublicationLogo = ({ isSidebar }: { isSidebar?: boolean }) => {
-	const { publication } = useAppContext();
-	const PUBLICATION_LOGO = getPublicationLogo(publication, isSidebar);
+  const { publication } = useAppContext();
+  const PUBLICATION_LOGO = getPublicationLogo(publication, isSidebar);
 
-	return (
-		<h1 className="relative w-full">
-			<Link
-				href={'/'}
-				aria-label={`${publication.title} blog home page`}
-				className="flex flex-row items-center justify-center gap-3"
-			>
-				{PUBLICATION_LOGO ? (
-					<>
-						<img
-							className="block w-32 shrink-0 md:w-40"
-							alt={publication.title}
-							src={resizeImage(PUBLICATION_LOGO, { w: 320, h: 80 })}
-						/>
-						<span className="text-2xl font-semibold text-white md:text-3xl">Blog</span>
-					</>
-				) : (
-					<span
-						className={`block text-2xl font-semibold ${
-							isSidebar ? 'text-black dark:text-white' : 'text-white md:text-4xl'
-						}`}
-					>
-						{publication.title}
-					</span>
-				)}
-			</Link>
-		</h1>
-	);
+  return (
+    <h1 className="relative w-full">
+      <Link
+        href={'/'}
+        aria-label={`${publication.title} blog home page`}
+        className="flex flex-row items-center justify-center gap-3"
+      >
+        {PUBLICATION_LOGO ? (
+          <>
+            <img
+              className="block w-32 shrink-0 md:w-44 lg:w-56"
+              alt={publication.title}
+              src={resizeImage(PUBLICATION_LOGO, { w: 320, h: 80 })}
+            />
+          </>
+        ) : (
+          <span
+            className={`block text-2xl font-semibold ${isSidebar ? 'text-black dark:text-white' : 'text-black dark:text-white md:text-4xl'
+              }`}
+          >
+            {publication.title}
+          </span>
+        )}
+      </Link>
+    </h1>
+  );
 };
